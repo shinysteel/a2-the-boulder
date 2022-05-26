61c76d55320ec9be8ed0f13c5c0ac67d13570311
n10459839 <68493919+n10459839@users.noreply.github.com>
2022-05-27 00:21:46 +1000

    Fall System, Health and Respawn System
    Needs testing and polish

--- IGB388 Assignment A/Assets/Aidan's Files/Scripts/Player/Player.cs
+++ IGB388 Assignment A/Assets/Aidan's Files/Scripts/Player/Player.cs
@@ -11,29 +11,116 @@ namespace Assignment
 
         private PlayerMovement movement;
         private PlayerClimbing climbing;
-        private void Start()
+
+        //Make a boolean ground check for falling
+        bool is_grounded = false;
+        bool was_grounded;
+        bool was_falling;
+        float start_of_fall;
+        bool is_falling { get { return (!is_grounded && GetComponent<Rigidbody>().velocity.y < 0); } }
+        public float minimum_fall = 2f;
+
+        //Health variables
+        public float max_health = 50f;
+        float current_health;
+        public float health_regen = 1f;
+        bool regen = false;
+
+        //Respawn locations
+        public Transform respawn_location;
+        public Transform start_location;
+
+        void Start()
         {
             movement = GetComponent<PlayerMovement>();
             climbing = GetComponent<PlayerClimbing>();
 
             climbing.onClimbStart += OnClimbStart;
             climbing.onClimbEnd += OnClimbEnd;
+
+            current_health = max_health;
         }
 
-        private void OnClimbStart()
+        void Update()
+        {
+            CheckGround();
+
+            if (!was_falling && is_falling)
+            {
+                start_of_fall = transform.position.y;
+            }
+
+            if (!was_grounded && is_grounded)
+            {
+                TakeDamage();
+
+                if (current_health <= 0)
+                {
+                    Respawn();
+                }
+            }
+
+            //Checks last frame if player was in the air
+            was_grounded = is_grounded;
+            was_falling = is_falling;
+
+            if (!regen)
+            {
+                StartCoroutine(RegenHealth());
+            }
+        }
+
+        void OnClimbStart()
         {
             UpdateState(eState.Climbing);
         }
-        private void OnClimbEnd()
+
+        void OnClimbEnd()
         {
             UpdateState(eState.Grounded);
         }
 
-
-        private void UpdateState(eState newState)
+        void UpdateState(eState newState)
         {
             state = newState;
             movement.enabled = state == eState.Grounded;
+        }
+
+        //Raycast for player touching the ground
+        void CheckGround()
+        {
+            is_grounded = Physics.Raycast(transform.position + Vector3.up, -Vector3.up, 1.01f);
+        }
+
+        //Deal damage based on distance fallen
+        void TakeDamage()
+        {
+            float fall_distance = start_of_fall - transform.position.y;
+
+            if (fall_distance > minimum_fall)
+            {
+                current_health -= fall_distance;
+            }
+        }
+
+        IEnumerator RegenHealth()
+        {
+            regen = true;
+
+            yield return new WaitForSeconds(1f);
+
+            current_health += health_regen;
+
+            regen = false;
+        }
+
+        void Respawn()
+        {
+            //Restore all health to the player
+            current_health = max_health;
+
+            //Teleport player to the save points throughout the map
+            transform.position = respawn_location.position;
         }
     }
 }
---
+++ IGB388 Assignment A/Assets/Checkpoint.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assignment;
+
+public class Checkpoint : MonoBehaviour
+{
+    public GameObject waypoint;
+    Player player;
+    public List<GameObject> special_waypoints = new List<GameObject>();
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.respawn_location = waypoint.transform;
+
+            if (!special_waypoints.Contains(waypoint))
+            {
+                gameObject.SetActive(false);
+            }
+
+            if (name == "Checkpoint 8")
+            {
+                foreach (GameObject waypoint in special_waypoints)
+                {
+                    waypoint.SetActive(false);
+                }
+            }
+        }
+    }
+}
